71bcc17b9e5cd28fa0a48e8c4089042c69e2973f
Valentina Werle <valentinawerle@gmail.com>
2024-04-22 20:22:52 -0300

    coneccion a base de datos

--- ModeloMVC_py/todoItem.py
+++
@@ -1,9 +0,0 @@
-class TodoItem:
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
-        self.completed = False
-
-    def __str__(self):
-        status = "Completada" if self.completed else "Pendiente"
-        return f"{self.title} - ({status})"
