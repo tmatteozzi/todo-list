3641915e04b5b8d4415430c9baeb0af8f8745aad
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-04-30 18:06:55 -0300

    config commit

--- ModeloMVC_py/config.py
+++ ModeloMVC_py/config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import OperationalError
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:Hola123@localhost:3306/todoApp"
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:Root123@localhost:3306/todoApp"
 
 try:
     engine = create_engine(SQLALCHEMY_DATABASE_URI)
