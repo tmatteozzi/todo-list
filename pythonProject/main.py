05f18b63efb6ebc26e8b0fb7d8fd8b6e6893623f
Matias Marzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2024-06-21 21:17:14 -0300

    subida del trabajo 2.0

---
+++ pythonProject/main.py
@@ -0,0 +1,52 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    tasks = []
+
+    def _set_headers(self, content_type="text/html"):
+        self.send_response(200)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+
+    def do_GET(self):
+        if self.path == '/':
+            self.serve_html()
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+    def serve_html(self):
+        html_file_path = "index.html"
+        try:
+            with open(html_file_path, 'rb') as file:
+                self._set_headers()
+                self.wfile.write(file.read())
+        except FileNotFoundError:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+    def do_POST(self):
+        if self.path == '/add_task':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            task_data = json.loads(post_data)
+
+            task = {
+                'task': task_data['task'],
+                'description': task_data['description']
+            }
+            self.tasks.append(task)
+
+            self._set_headers("application/json")
+            self.wfile.write(json.dumps(task).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
